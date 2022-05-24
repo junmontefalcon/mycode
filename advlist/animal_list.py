5cef228b75bf6333732178279e2049d9bf0e42d4
Jun Montefalcon <junmontefalcon@users.noreply.github.com>
2022-05-24 18:58:46 +0000

    About complex lists

---
+++ advlist/animal_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def main():
+    # create a list called list1
+    animal_list1= ["Fox", "Fly","Ant","Bee","Cod","Cat","Dog","Yak","Cow","Hen","Ko","Hog","Jay","Kit"]
+    
+    # display the animal_list1 
+    print(animal_list1)
+
+main()
